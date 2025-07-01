42659e1a785b5d791cc5b3635423763d4ed88f47
Kartik Diwan <kartikdiwan99@gmail.com>
2025-07-02 00:52:57 +0530

    Update submit.py

--- submit.py
+++ submit.py
@@ -3,34 +3,7 @@ import sklearn
 from sklearn.svm import LinearSVC
 from scipy.linalg import khatri_rao
 
-# You are allowed to import any submodules of sklearn that learn linear models e.g. sklearn.svm etc
-# You are not allowed to use other libraries such as keras, tensorflow etc
-# You are not allowed to use any scipy routine other than khatri_rao
-
-# SUBMIT YOUR CODE AS A SINGLE PYTHON (.PY) FILE INSIDE A ZIP ARCHIVE
-# THE NAME OF THE PYTHON FILE MUST BE submit.py
-
-# DO NOT CHANGE THE NAME OF THE METHODS my_fit, my_map, my_decode etc BELOW
-# THESE WILL BE INVOKED BY THE EVALUATION SCRIPT. CHANGING THESE NAMES WILL CAUSE EVALUATION FAILURE
-
-# You may define any new functions, variables, classes here
-# For example, functions to calculate next coordinate or step length
-
-################################
-# Non Editable Region Starting #
-################################
 def my_fit(X_train, y_train):
-################################
-#  Non Editable Region Ending  #
-################################
-
-	# Use this method to train your models using training CRPs
-	# X_train has 8 columns containing the challenge bits
-	# y_train contains the values for responses
-	
-	# THE RETURNED MODEL SHOULD BE ONE VECTOR AND ONE BIAS TERM
-	# If you do not wish to use a bias term, set it to 0
-
 	X_train_mapped = my_map(X_train)
 	model = LinearSVC(
 		loss='hinge',         
@@ -45,17 +18,7 @@ def my_fit(X_train, y_train):
 
 	return w, b
 
-
-################################
-# Non Editable Region Starting #
-################################
 def my_map(X):
-################################
-#  Non Editable Region Ending  #
-################################
-
-	# Use this method to create features.
-	# It is likely that my_fit will internally call my_map to create features for train points
 	X = 2 * X - 1
 	n_samples, n_bits = X.shape
 	phi = np.zeros((n_samples, n_bits + 1))
@@ -70,18 +33,7 @@ def my_map(X):
 
 	return feat
 
-
-################################
-# Non Editable Region Starting #
-################################
 def my_decode(w):
-################################
-#  Non Editable Region Ending  #
-################################
-
-	# Use this method to invert a PUF linear model to get back delays
-	# w is a single 65-dim vector (last dimension being the bias term)
-	# The output should be four 64-dimensional vectors
 	weights = w[:-1]
 	bias = w[-1]
 
